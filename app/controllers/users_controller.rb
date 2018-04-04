4889a84d4ad0334a75f383ca6de4a2bde346a924
Manuela Solís <manuelaymanuela@hotmail.com>
2018-04-04 10:45:38 +0200

    users_controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -28,11 +28,8 @@ class UsersController < ApplicationController
 
     respond_to do |format|
       if @user.save
-        format.html { redirect_to @user, notice: 'User was successfully created.' }
-        format.json { render :show, status: :created, location: @user }
-      else
-        format.html { render :new }
-        format.json { render json: @user.errors, status: :unprocessable_entity }
+        # Dile al mailer que envíe un correo de bienvenida después de guardar
+        UserMailer.welcome_email(@user).deliver_now
       end
     end
   end
