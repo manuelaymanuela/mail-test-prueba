0b5e0f546553de00d75c5d7cecf87ce194f06efd
Manuela Solís <manuelaymanuela@hotmail.com>
2018-04-04 11:27:05 +0200

    test/mailers/previews/user_mailer_preview.rb

--- test/mailers/previews/user_mailer_preview.rb
+++ test/mailers/previews/user_mailer_preview.rb
@@ -1,4 +1,7 @@
-# Preview all emails at http://localhost:3000/rails/mailers/user_mailer
 class UserMailerPreview < ActionMailer::Preview
+# Vista previa de todos los emails en http://localhost:3000/rails/mailers/user_mailer
 
-end
+    def user_mail_preview
+      UserMailer.welcome_email(User.first)
+    end
+  end
